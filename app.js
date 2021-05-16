e52f4f9345637e0a176bb0e99aef5f8a3555267e
Aurelie75FR <aurelielo.vi@gmail.com>
2021-05-16 20:49:20 +0200

    restart routes on collection

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var hbs = require('hbs')
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -15,6 +17,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+// hbs.registerPartial(__dirname, "views/collection")
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -26,6 +29,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/collection", collectionRouter);
+//app.use("/card", require('./routes/card))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/collection.js
+++ routes/collection.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const router = new express.Router();
 //models en veux-tu en voilà
-//const unModel = require("/unmodel");
+const collectionModel= require("../models/model.collection");
 
 //CRUD
 
 //GET ALL
-router.get("/collection", (req, res, next) => {
-  unModel.find().populate("quelquechose")
-    .then(() => res.render("collection"))
+router.get("/", (req, res, next) => {
+  collectionModel.find()
+  //.populate("quelquechose")
+    .then((result) => res.render("collection", {collection : result}))
     .catch(next);
 });
 
 // GET CREATE
 router.get("/add-collection", (req, res, next) => {
-  unOuDesModel.find()
+  collectionModel.find()
     .then((result) => res.render("collection/add-collection", { newCollection: result }))
     .catch(next);
 });
 
 //GET Update
 router.get("/update-collection/:id", (req, res, next) => {
-  unOuDesModel.findById(req.params.id).populate("quelquechose")
+  collectionModel.findById(req.params.id).populate("quelquechose")
     .then((result) =>
       res.render("collection/update-collection", { unOuDesModel: result })
     )
@@ -30,26 +31,26 @@ router.get("/update-collection/:id", (req, res, next) => {
 
 //GET One Card
 router.get("/collection/:id", (req, res, next) => {
-  unModel.findById(req.params.id)
+  collectionModel.findById(req.params.id)
     .then((card) => res.render("collection/Onecard", { OneCardDetail: card }))
     .catch(next);
 });
 
 //GET Delete
 router.get("/delete/:id", (req, res, next) => {
-  unModel.findByIdAndRemove(req.params.id)
+  collectionModel.findByIdAndRemove(req.params.id)
     .then(() => res.redirect("/dashboard/collection"))
     .catch(next);
 });
 //POST CREATE
 router.post("/create", (req, res, next) => {
-  unModel.create(req.body)
+  collectionModel.create(req.body)
     .then(() => res.redirect("/dashboard/collection"))
     .catch(next);
 });
 //POST Update
 router.post("/update/:id", (req, res, next) => {
-  unModel.findByIdAndUpdate(req.params.id, req.body)
+  collectionModel.findByIdAndUpdate(req.params.id, req.body)
     .then(() => res.redirect("/dashboard/collection"))
     .catch(next);
 });
