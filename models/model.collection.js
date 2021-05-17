fe4d7376944db84c1e99bd52d4d53ae56c851699
Souchard <bettina.souchard@gmail.com>
2021-05-17 12:00:09 +0200

    updates

--- models/model.collection.js
+++ models/model.collection.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const CollectionSchema = new Schema(
   {
     name: String,
-    image: String,
+    image: {type:String, default:"https://images.unsplash.com/photo-1500673587002-1d2548cfba1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"},
     description: String,
     cards: [{type: Schema.Types.ObjectId, ref: "card"}],
   },
